7fb56bd5ad8bdfe74e86e0f1c8b520c8d5663a49
PythonCakeMing <1851464029@qq.com>
2017-03-01 16:51:59 +0800

    video logs

---
+++ database/migrations/2017_03_01_082508_create_video_logs_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('video_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->comment('用户id');
+            $table->unsignedInteger('application_id')->comment('项目id');
+
+            $table->string("video_name")->comment("视频名字");
+            $table->string("video_labels")->comment("视频标签, 可能含多个");
+
+            $table->dateTime("time_start")->comment("视频播放开始时间");
+            $table->decimal("time_span", 10, 2)->comment("视频播放长度, 分钟单位");
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('md_users');
+            $table->foreign('application_id')->references('id')->on('applications');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('video_logs', function (Blueprint $table) {
+            $table->dropForeign('video_logs_user_id_foreign');
+            $table->dropForeign('video_logs_application_id_foreign');
+        });
+        Schema::dropIfExists('video_logs');
+    }
+}
