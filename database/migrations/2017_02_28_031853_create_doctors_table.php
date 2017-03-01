6981eb938dbc66a813573e92a5902e31481287c7
PythonCakeMing <1851464029@qq.com>
2017-03-01 10:35:27 +0800

    md doctors

---
+++ database/migrations/2017_02_28_031853_create_doctors_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title')->unique()->comment('医生职称');
+            $table->string('office')->nullable()->comment('医生科室');
+
+            $table->integer('user_id')->unsigned()->comment('用户id');
+            $table->integer('hospital_id')->unsigned()->comment('医院id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('md_users');
+            $table->foreign('hospital_id')->references('id')->on('hospitals');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->dropForeign('doctors_user_id_foreign');
+            $table->dropForeign('doctors_hospital_id_foreign');
+        });
+        Schema::drop('doctors');
+    }
+}
