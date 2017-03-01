2b3a6432011a05a620a8f369139656f693902949
PythonCakeMing <1851464029@qq.com>
2017-03-01 10:45:27 +0800

    novo volunteers

---
+++ database/migrations/2017_02_28_061540_create_novo_volunteers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNovoVolunteersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('novo_volunteers', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('initial')->nullable()->comment('代表initial');
+            $table->string('region')->nullable()->comment('代表所在大区');
+            $table->integer('user_id')->unsigned()->comment('用户id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('md_users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('novo_volunteers', function (Blueprint $table) {
+            $table->dropForeign('novo_volunteers_user_id_foreign');
+        });
+        Schema::drop('novo_volunteers');
+    }
+}
