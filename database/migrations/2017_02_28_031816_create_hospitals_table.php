03017c730743093a7d82f4da35f8b4e92948c142
PythonCakeMing <1851464029@qq.com>
2017-03-01 10:27:39 +0800

    md hospitals

---
+++ database/migrations/2017_02_28_031816_create_hospitals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHospitalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name')->unique()->comment('医院名字');
+            $table->string('type')->nullable()->comment('医院类型');
+
+            $table->string('province')->nullable()->comment('省');
+            $table->string('city')->nullable()->comment('市');
+            $table->string('area')->nullable()->comment('区');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('hospitals');
+    }
+}
